e5381996597f53f7e3b8667fb1be87a38119479f
James-Burch <burchjames6@gmail.com>
2025-03-11 00:39:22 +0000

    Add background shapes and section seperators to app page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,66 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact"
+import BackgroundShape from "./components/common/BackgroundShape";
+import SectionDivider from "./components/common/SectionDivider";
 import { DarkThemeProvider } from './context/DarkTheme';
 import "./index.css";
 
+
+// const App = () => {
+//   const [activeSection, setActiveSection] = useState("home");
+
+//   const navigateToSection = (sectionId) => {
+//     setActiveSection(sectionId);
+//     const element = document.getElementById(sectionId);
+//     if (element) {
+//       element.scrollIntoView({ behavior: "smooth" });
+//     }
+//   };
+
+//   return (
+//     <DarkThemeProvider>
+//       <div className="app">
+//         <Navbar activeSection={activeSection} onNavigate={navigateToSection} />
+//         <main>
+//           <section id="home">
+//             <Hero onNavigate={navigateToSection} />
+//           </section>
+
+//           <div className="container">
+//             <SectionDivider style="gradient" />
+//           </div>
+
+//           <section id="about">
+//             <About />
+//           </section>
+
+//           <div className="container">
+//             <SectionDivider style="gradient" />
+//           </div>
+
+//           <section id="projects">
+//             <Projects />
+//           </section>
+
+//           <div className="container">
+//             <SectionDivider style="gradient" />
+//           </div>
+
+//           <section id="contact">
+//             <Contact />
+//           </section>
+//         </main>
+
+//         <footer className="main-footer">
+//           <div className="container">
+//             <p>&copy; {new Date().getFullYear()} James Burch. All rights reserved.</p>
+//           </div>
+//         </footer>
+//       </div>
+//     </DarkThemeProvider>
+//   );
+// };
 
 const App = () => {
   const [activeSection, setActiveSection] = useState("home");
@@ -22,26 +79,152 @@ const App = () => {
   return (
     <DarkThemeProvider>
       <div className="app">
+        {/* Hero section shapes */}
+        <BackgroundShape 
+          type="triangle" 
+          position="top-left" 
+          customStyles={{ 
+            top: "150px", 
+            left: "5%", 
+            opacity: "0.06",
+            position: "absolute" 
+          }} 
+        />
+        <BackgroundShape 
+          type="circle" 
+          position="top-right" 
+          customStyles={{ 
+            top: "100px", 
+            right: "5%", 
+            width: "250px",
+            height: "250px",
+            opacity: "0.06",
+            position: "absolute" 
+          }} 
+        />
+        <BackgroundShape 
+          type="circle" 
+          position="bottom-right" 
+          customStyles={{ 
+            top: "500px", 
+            right: "15%", 
+            width: "180px",
+            height: "180px",
+            opacity: "0.06",
+            position: "absolute" 
+          }} 
+        />
+
         <Navbar activeSection={activeSection} onNavigate={navigateToSection} />
         <main>
           <section id="home">
             <Hero onNavigate={navigateToSection} />
           </section>
 
-          <section id="about">
+          <section id="about" style={{ position: "relative" }}>
+            {/* About section shapes */}
+            <BackgroundShape 
+              type="square" 
+              position="top-right" 
+              customStyles={{ 
+                top: "50px", 
+                right: "8%", 
+                opacity: "0.06",
+                position: "absolute",
+                zIndex: "0" 
+              }} 
+            />
+            <BackgroundShape 
+              type="donut" 
+              position="bottom-left" 
+              customStyles={{ 
+                bottom: "80px", 
+                left: "10%", 
+                opacity: "0.06",
+                position: "absolute",
+                zIndex: "0" 
+              }} 
+            />
             <About />
           </section>
 
-          <section id="projects">
+          <section id="projects" style={{ position: "relative" }}>
+            {/* Projects section shapes */}
+            <BackgroundShape 
+              type="circle" 
+              position="top-left" 
+              customStyles={{ 
+                top: "100px", 
+                left: "7%", 
+                width: "120px",
+                height: "120px",
+                opacity: "0.05",
+                position: "absolute",
+                zIndex: "0" 
+              }} 
+            />
+            <BackgroundShape 
+              type="triangle" 
+              position="bottom-right" 
+              customStyles={{ 
+                bottom: "150px", 
+                right: "5%", 
+                opacity: "0.04",
+                position: "absolute",
+                zIndex: "0",
+                transform: "rotate(180deg)" 
+              }} 
+            />
             <Projects />
           </section>
 
-          <section id="contact">
+          <section id="contact" style={{ position: "relative" }}>
+            {/* Contact section shapes */}
+            <BackgroundShape 
+              type="square" 
+              position="top-right" 
+              customStyles={{ 
+                top: "80px", 
+                right: "10%", 
+                opacity: "0.05",
+                position: "absolute",
+                zIndex: "0",
+                transform: "rotate(30deg)" 
+              }} 
+            />
+            <BackgroundShape 
+              type="circle" 
+              position="bottom-left" 
+              customStyles={{ 
+                bottom: "100px", 
+                left: "8%", 
+                width: "150px",
+                height: "150px",
+                opacity: "0.06",
+                position: "absolute",
+                zIndex: "0" 
+              }} 
+            />
             <Contact />
           </section>
         </main>
 
-        <footer className="main-footer">
+        <footer className="main-footer" style={{ position: "relative" }}>
+          {/* Footer shape */}
+          <BackgroundShape 
+            type="donut" 
+            position="mid-right" 
+            customStyles={{ 
+              top: "50%", 
+              right: "15%", 
+              transform: "translateY(-50%)",
+              width: "100px",
+              height: "100px",
+              opacity: "0.05",
+              position: "absolute",
+              zIndex: "0" 
+            }} 
+          />
           <div className="container">
             <p>&copy; {new Date().getFullYear()} James Burch. All rights reserved.</p>
           </div>
